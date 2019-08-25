8dc675b386ebbe65932bdadc653ce5371054d42f
Daniel Martín <mardani29@yahoo.es>
2019-08-25 20:01:52 +0200

    Improve package description

--- swift-helpful.el
+++ swift-helpful.el
@@ -23,8 +23,17 @@
 
 ;;; Commentary:
 
-;; A minor mode that brings a self-documenting environment for the
-;; Swift programming language.
+;; This extension adds a command, `swift-helpful', which, when
+;; executed, shows documentation about a Swift token under
+;; point.  Sources of documentation include:
+;;
+;;    - Swift official manual and reference manual (installed by this
+;;    extension in GNU Info format).
+;;
+;;    - Language Server Protocol (LSP), if configured for the project.
+;;
+;;    - Swift standard library source code (if available locally and
+;;    its path is correctly configured).
 
 ;;; Code:
 
